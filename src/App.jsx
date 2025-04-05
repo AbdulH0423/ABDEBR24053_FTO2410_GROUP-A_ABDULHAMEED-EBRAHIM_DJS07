291f7dc808e8d781a2771e179da1277c11f18e30
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-04-05 13:42:45 +0200

    Imported meme and header components into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from "./Components/Header.jsx"
+import Meme from "./Components/Meme.jsx";
 
 function App() {
 
   return (
-    <>
+    <div>
+      <Header />
+      <Meme />
      
-    </>
+    </div>
   )
 }
 
